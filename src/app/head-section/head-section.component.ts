6b4253145f5059ffc1eca239995aca604bdebc1c
StayAbroad <dennis.koester11@gmail.com>
2023-02-15 00:20:50 +0100

    header

---
+++ src/app/head-section/head-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-section',
+  templateUrl: './head-section.component.html',
+  styleUrls: ['./head-section.component.scss']
+})
+export class HeadSectionComponent {
+
+}
