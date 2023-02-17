be0e14a72942305450517374625cb5fb53df4120
StayAbroad <dennis.koester11@gmail.com>
2023-02-17 02:02:10 +0100

    about me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeadSectionComponent } from './head-section/head-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeadSectionComponent
+    HeadSectionComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
