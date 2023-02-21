2521c3a37e87daac547848cb573bd23225cfe2a9
StayAbroad <dennis.koester11@gmail.com>
2023-02-22 00:01:04 +0100

    animation frames fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    ngOnInit() {
+        var header = document.getElementById('header');
 
+        onscroll = (event) => {
+            if (header.offsetTop !== 0) {
+                if (!header.classList.contains('shadow')) {
+                    header.classList.add('shadow');
+                    console.log('at top');
+                    console.log(header.offsetTop);
+                    
+                }
+            } else {
+                header.classList.remove('shadow');
+                console.log('not at top');
+                    console.log(header.offsetTop);
+            }
+        };
+    }
 }
