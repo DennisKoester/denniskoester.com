37a552e2239d5a6b3c0f4b4362feac4332db3254
StayAbroad <dennis.koester11@gmail.com>
2023-02-26 19:14:23 +0100

    work on hidden scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'denniskoester.com';
+    title = 'denniskoester.com';
+
+    timer = 500;
+
+    @HostListener('window:scroll', []) onWindowScroll() {
+        document.body.classList.remove('hideScrollbar');
+        console.log('visiable');
+        this.timer && clearTimeout(this.timer);
+        this.timer = setTimeout(() => {
+            document.body.classList.add('hideScrollbar');
+            console.log('hidden');
+        }, 500);
+    }
 }
