8564d533206b450ac9a5b0ee844aa8324c3ee34e
StayAbroad <dennis.koester11@gmail.com>
2023-02-27 23:34:15 +0100

    copyright date function

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+    currentYear: number;
+
+    ngOnInit() {
+        this.generateDate();
+    }
+
+    generateDate() {
+        this.currentYear = Date.now();
+    }
+
     scrollToTop() {
         window.scroll({
             top: 0,
