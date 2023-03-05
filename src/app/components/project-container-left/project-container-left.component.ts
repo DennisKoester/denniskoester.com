17528989e162847ead33ad17330fa7afd70a0d3c
StayAbroad <dennis.koester11@gmail.com>
2023-03-05 22:58:17 +0100

    work on json projects

--- src/app/components/project-container-left/project-container-left.component.ts
+++ src/app/components/project-container-left/project-container-left.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-project-container-left',
-  templateUrl: './project-container-left.component.html',
-  styleUrls: ['./project-container-left.component.scss']
+    selector: 'app-project-container-left',
+    templateUrl: './project-container-left.component.html',
+    styleUrls: ['./project-container-left.component.scss'],
 })
 export class ProjectContainerLeftComponent {
+    projects = [
+        {
+            name: 'Join',
+            subheadline: 'Angular | Typescript | HTML | CSS | Firebase',
+            desc: ' Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+            image: 'join-project.png',
+        },
 
+        {
+            name: 'Pokedex',
+            subheadline: 'Javascript | HTML | CSS | Rest API',
+            desc: ' Pokedex using Rest API',
+            image: 'join-project.png',
+        },
+    ];
 }
