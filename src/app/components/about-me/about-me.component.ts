f8512b30a34848a8e2c53163ef34a966059e248b
StayAbroad <dennis.koester11@gmail.com>
2023-04-11 23:03:57 +0200

    work on animations

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavigateService } from 'src/app/navigate.service';
 
 @Component({
     selector: 'app-about-me',
     templateUrl: './about-me.component.html',
     styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+    constructor(public navigate: NavigateService) {}
+}
--- src/app/components/head-section/head-section.component.ts
+++ src/app/components/head-section/head-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NavigateService } from 'src/app/navigate.service';
 
 @Component({
-  selector: 'app-head-section',
-  templateUrl: './head-section.component.html',
-  styleUrls: ['./head-section.component.scss']
+    selector: 'app-head-section',
+    templateUrl: './head-section.component.html',
+    styleUrls: ['./head-section.component.scss'],
 })
 export class HeadSectionComponent {
-
+    constructor(public navigate: NavigateService) {}
 }
