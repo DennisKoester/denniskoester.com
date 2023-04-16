af13aec871437abf4f2a6ffa1e11d9cd2ee374ab
StayAbroad <dennis.koester11@gmail.com>
2023-04-16 22:11:05 +0200

    funny log

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+    ngOnInit() {
+        console.log(
+            '%cWelcome to my Portfolio',
+            'color: orange; font-size: 40px;'
+        );
+        console.log(
+            '%cFeel free to check out my code on www.github.com/DennisKoester',
+            'color: orange; font-size: 18px;'
+        );
+    }
 }
